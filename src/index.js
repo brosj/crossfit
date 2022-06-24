af6560d2b24518a87a6ce1adfd0ff8efc37f1f90
brosj <joshuaonyeuche@gmail.com>
2022-06-24 14:04:40 +0100

    Root x Router

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import React from "react";
 import reactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
+
+import App from "./App";
+
+const root = reactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
