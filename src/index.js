adb6f7d52bff22a49cc81e0ae8b83cf6cf269a33
brosj <joshuaonyeuche@gmail.com>
2022-06-26 14:18:08 +0100

    Switching root to ReactDOMClient

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import reactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
+import * as ReactDOMClient from 'react-dom/client';
 
 import App from "./App";
 
-const root = reactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
